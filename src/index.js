bf2b0513da9e25860a2aa0c68f886608c8958fc5
Nick Warner <nikolaiwarner@users.noreply.github.com>
2017-04-01 00:58:57 -0400

    Throw an error if HTML file is opened with `file://` protocol vs a local server or hosted (#2540)

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ if (document.currentScript && document.currentScript.parentNode !== document.hea
        'HTML.');
 }
 
+if (window.location.protocol === 'file:') {
+  warn('This HTML file is currently served via the file:// protocol. This does not ' +
+       'work well with assets or files fetched in the A-Frame scene. Consider serving ' +
+       'this file from a hosted or local server with a http:// or https:// protocol.');
+}
+
 // Polyfill `Promise`.
 window.Promise = window.Promise || require('promise-polyfill');
 
@@ -73,7 +79,7 @@ require('./core/a-mixin');
 require('./extras/components/');
 require('./extras/primitives/');
 
-console.log('A-Frame Version: 0.5.0 (Date 30-03-2017, Commit #75e40cf)');
+console.log('A-Frame Version: 0.5.0 (Date 01-04-2017, Commit #22c9ddb)');
 console.log('three Version:', pkg.dependencies['three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
 
